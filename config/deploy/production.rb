ee7e9903a9ef629801e0e8cb3ca1afe27763a2d1
kiai-nguyensontung <tung.nguyen@kiaisoft.com.vn>
2025-03-06 14:21:02 +0700

    add ec2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '10.0.3.193', user: 'ubuntu', roles: %w{db web app}
+server '3.92.1.101', user: 'ubuntu', roles: %w{db web app}
 set :puma_workers, 2
 
 
