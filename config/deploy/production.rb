af72fef2b134eae67ab2171c2218615f73cf6c44
nstung183 <nguyensontung183@gmail.com>
2025-03-06 22:58:32 +0700

    Capistrano deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '3.92.1.101', user: 'ubuntu', roles: %w{db web app}
+set :stage, :production
+set :rails_env, "production"
 
 # role-based syntax
 # ==================
