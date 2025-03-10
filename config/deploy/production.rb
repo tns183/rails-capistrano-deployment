a7ac2d10197d4042081f28d1d2014a0389988131
nstung183 <nguyensontung183@gmail.com>
2025-03-10 22:49:44 +0700

    Capistrano deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '3.92.1.101', user: 'ubuntu', roles: %w{db web app}
+server '34.204.1.149', user: 'ubuntu', roles: %w{db web app}
 set :stage, :production
 set :rails_env, "production"
 set :linked_files, %w{.env config/credentials/production.yml.enc}
